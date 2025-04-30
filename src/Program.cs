bbf4424b6151f1864b029cac43b38895b33bbe7b
fernandop-dev <fernandopuertof@gmail.com>
2025-04-29 21:11:11 -0600

    feat: agregar estructura del Program.cs

--- src/Program.cs
+++ src/Program.cs
@@ -1,41 +1,25 @@
-var builder = WebApplication.CreateBuilder(args);
+#region Step 1: Configuration Setup
+ var builder = WebApplication.CreateBuilder(args);
+#endregion Step 1: Configuration Setup
+    
+#region Step2: Service Registration
+#region Step2.1: Add services to the DI container.
+    
+#endregion Step2.1: Add services to the DI container.
+    
+#region Step2.2: Add database context
+    
+#endregion Step2.2: Add database context
+#endregion Step2: Service Registration
+    
+#region Step3: Build the application
+ var app = builder.Build();
+#endregion Step3: Build the application
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+#region Step4: Middleware Pipeline Configuration
 
-var app = builder.Build();
+#endregion Step4: Middleware Pipeline Configuration
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
-
-app.UseHttpsRedirection();
-
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-    {
-        var forecast = Enumerable.Range(1, 5).Select(index =>
-                new WeatherForecast
-                (
-                    DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                    Random.Shared.Next(-20, 55),
-                    summaries[Random.Shared.Next(summaries.Length)]
-                ))
-            .ToArray();
-        return forecast;
-    })
-    .WithName("GetWeatherForecast");
-
-app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
+#region Step5: Start the Application
+ app.Run();
+#endregion Step5: Start the Application
